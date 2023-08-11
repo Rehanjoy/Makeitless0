968415d70db22e2831a6d470300f9466021d36f6
Rehan <rehan101err0r@gmail.com>
2023-08-11 12:19:07 +0530

    Rehancommit

--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,38 @@ import HerroCategory from "@/components/home/HerroCategory";
 import Layout from "@/components/layout/Layout";
 import HeroTest1 from "@/components/home/HeroTest1";
 import LatestProduct from "@/components/home/LastestProduct";
-import CarouselTest1 from "@/components/home/CarouselTest1"
-import CarouselTest2 from "@/components/home/CarouselTest2";
+
 import OurProducts from "@/components/home/OurProduct";
 import Categories from "@/components/home/Categories";
 import Contact from "@/components/home/Contact";
-import NewsLetter from "@/components/home/NewLetter";
-import WhatOffers from "@/components/home/WhatOffers"
+
+import WhatOffers from "@/components/home/WhatOffers";
 import Bannermostselling from "@/components/Banner/Bannermostselling";
 import Bannerproducts from "@/components/Banner/Bannerproducts";
 import Bannernewarrival from "@/components/Banner/Bannernewarrival";
+
+
 export default function Home() {
   return (
-<>
-<Layout>
-{/* 
+    <>
+      <Layout>
+        {/* 
 <CarouselTest2/>
 <CarouselTest1/>
 <NewsLetter/>
 */}
-<HerroCarousel />
-<HerroCategory />
-<HeroTest1/>
-<Bannerproducts/>
-<Categories/>
-<Bannermostselling/>
-<LatestProduct/>
-<WhatOffers/>
-<Bannernewarrival/>
-<OurProducts/>
-<Contact/> 
-</Layout>
-</>
-  )
+        <HerroCarousel />
+        <HerroCategory/>
+        <HeroTest1 />
+        <Bannerproducts />
+        <Categories />
+        <Bannermostselling />
+        <LatestProduct />
+        <WhatOffers />
+        <Bannernewarrival />
+        <OurProducts />
+        <Contact />
+      </Layout>
+    </>
+  );
 }
